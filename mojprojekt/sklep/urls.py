b40726a7b33bdc4fda8fcc2d4980180f43526666
Jan Michalski <jm99917@sgh.waw.pl>
2019-11-27 00:07:50 +0100

    Praca domowa 2

--- mojprojekt/sklep/urls.py
+++ mojprojekt/sklep/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("products/", views.product_list),
+    path("product/<int:id>", views.product_details),
     path("widok/<str:tekst>", views.index)
 ]
--- mojprojekt/sklep/views.py
+++ mojprojekt/sklep/views.py
@@ -6,10 +6,20 @@ products = [
     {
         "name": "Film",
         "price": 100,
-        "id": 1
+        "id": 1,
+        "description": "Mam nowy fantastyczny film - \"Śmierć w Wenecji\". Dobre, nie?",
+        "weight": "300 grams"
     },
-    {"name": "Ksiazka", "price": 30, "id": 2},
-    {"name": "Plyta", "price": 60, "id": 3},
+    {
+        "name": "Ksiazka", "price": 30, "id": 2,
+        "description": "Jakaś generyczna książka o coachingu i challengowaniu????",
+        "weight": "1 kg"
+    },
+    {
+        "name": "Plyta", "price": 60, "id": 3,
+        "description": "Metal subtelnie napieprza z głośników...",
+        "weight": "100 grams"
+    },
 ]
 
 
@@ -18,6 +28,20 @@ def product_list(request):
         request,
         "sklep/list.html",
         {"products": products}
+    )
+
+
+def product_details(request, id):
+    product = None
+    # Find product that has the given ID
+    for p in products:
+        if p['id'] == id:
+            product = p
+
+    return render(
+        request,
+        "sklep/details.html",
+        {"product": product}
     )
 
 
