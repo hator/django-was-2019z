caffdeb3770f5520fd4f2cf56f8baa0c168fd1f0
Jan Michalski <jan.michalski@codilime.com>
2019-11-29 11:21:34 +0100

    Zajecia 3

--- mojprojekt/sklep/views.py
+++ mojprojekt/sklep/views.py
@@ -1,47 +1,25 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
-
-products = [
-    {
-        "name": "Film",
-        "price": 100,
-        "id": 1,
-        "description": "Mam nowy fantastyczny film - \"Śmierć w Wenecji\". Dobre, nie?",
-        "weight": "300 grams"
-    },
-    {
-        "name": "Ksiazka", "price": 30, "id": 2,
-        "description": "Jakaś generyczna książka o coachingu i challengowaniu????",
-        "weight": "1 kg"
-    },
-    {
-        "name": "Plyta", "price": 60, "id": 3,
-        "description": "Metal subtelnie napieprza z głośników...",
-        "weight": "100 grams"
-    },
-]
+from .models import Product
 
 
 def product_list(request):
+    products = Product.objects.order_by('id')
+    context = {'products': products}
+
     return render(
         request,
         "sklep/list.html",
-        {"products": products}
+        context
     )
 
 
-def product_details(request, id):
-    product = None
-    # Find product that has the given ID
-    for p in products:
-        if p['id'] == id:
-            product = p
-
+def product_details(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {"product": product}
     return render(
         request,
         "sklep/details.html",
-        {"product": product}
+        context
     )
 
 
